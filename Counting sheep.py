506ac240508580c5d0ffc2f227d44694d9124a2e
fixCastro <37239241+fixCastro@users.noreply.github.com>
2022-05-14 00:33:39 -0300

    Count Sheep

---
+++ Counting sheep.py
@@ -0,0 +1,18 @@
+def count_sheeps(sheep: list):
+    sheep_count = 0
+    for i in range(len(sheep)):
+        if sheep[i] is True:
+            sheep_count += 1
+    return sheep_count
+
+def one_line_count_sheep(sheep: list): return sheep.count(True)
+
+array1 = [True,  True,  True,  False,
+          True,  True,  True,  True ,
+          True,  False, True,  False,
+          True,  False, False, True ,
+          True,  True,  True,  True ,
+          False, False, True,  True ]
+
+print(count_sheeps(array1))
+print(one_line_count_sheep(array1))
